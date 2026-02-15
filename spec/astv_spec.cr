3cfb26b4a49fb9d4ade89ffc0a8a51a584be9439
kojix2 <2xijok@gmail.com>
2026-02-15 21:38:22 +0900

    Add various fixture files for testing

--- spec/astv_spec.cr
+++ spec/astv_spec.cr
@@ -1,9 +1,24 @@
 require "./spec_helper"
+require "../src/astv/core"
+require "json"
 
-describe Astv do
-  # TODO: Write tests
+describe Astv::Core do
+  describe "#parse_response" do
+    Dir.glob("spec/fixtures/*.cr").each do |fixture_file|
+      basename = File.basename(fixture_file, ".cr")
+      expected_file = "spec/fixtures/#{basename}.json"
 
-  it "works" do
-    false.should eq(true)
+      it "correctly parses #{basename}.cr" do
+        source = File.read(fixture_file)
+        actual_json = Astv::Core.parse_response(source)
+        expected_json = File.read(expected_file)
+
+        # Parse both JSONs to compare as objects (ignoring formatting differences)
+        actual = JSON.parse(actual_json)
+        expected = JSON.parse(expected_json)
+
+        actual.should eq(expected)
+      end
+    end
   end
 end
